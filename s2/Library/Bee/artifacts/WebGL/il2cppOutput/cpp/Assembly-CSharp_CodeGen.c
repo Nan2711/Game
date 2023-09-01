464e5155cf86b0420223bad2c433e2fbba7c209f
Nan2711 <55702748+Nan2711@users.noreply.github.com>
2023-09-01 02:34:55 -0700

    v2

---
+++ s2/Library/Bee/artifacts/WebGL/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,74 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern void HelloWorld_Start_m7D4140BFC9DCC632B4B2907C1BFFAC4FF2FE9247 (void);
+extern void HelloWorld_Update_mD199B6A4EF418F7EBEE4AEC5645BB7B6215371C5 (void);
+extern void HelloWorld__ctor_m7E681D25064FF84A25730DDBC04091394DE55C35 (void);
+extern void Roll_Start_m498D28B15CC11478C9134312063294BA77BE6DE5 (void);
+extern void Roll_Update_m051DDBCBFB209C650F9FB53515CE193B21A677A1 (void);
+extern void Roll__ctor_m575E00FAEC2AA453497988FFC01AA145665538D0 (void);
+extern void pp_Start_mD7D10C3146BC9D6BE626832C2F727771BDA0CF2B (void);
+extern void pp_Update_m7FF7637C4F2341A0A53AD69B12D99B9C3607CB52 (void);
+extern void pp__ctor_mBFB36703604CFA72B2D67980F287BC0BB0733E48 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1 (void);
+static Il2CppMethodPointer s_methodPointers[11] = 
+{
+	HelloWorld_Start_m7D4140BFC9DCC632B4B2907C1BFFAC4FF2FE9247,
+	HelloWorld_Update_mD199B6A4EF418F7EBEE4AEC5645BB7B6215371C5,
+	HelloWorld__ctor_m7E681D25064FF84A25730DDBC04091394DE55C35,
+	Roll_Start_m498D28B15CC11478C9134312063294BA77BE6DE5,
+	Roll_Update_m051DDBCBFB209C650F9FB53515CE193B21A677A1,
+	Roll__ctor_m575E00FAEC2AA453497988FFC01AA145665538D0,
+	pp_Start_mD7D10C3146BC9D6BE626832C2F727771BDA0CF2B,
+	pp_Update_m7FF7637C4F2341A0A53AD69B12D99B9C3607CB52,
+	pp__ctor_mBFB36703604CFA72B2D67980F287BC0BB0733E48,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1,
+};
+static const int32_t s_InvokerIndices[11] = 
+{
+	1098,
+	1098,
+	1098,
+	1098,
+	1098,
+	1098,
+	1098,
+	1098,
+	1098,
+	2121,
+	1098,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	11,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+};
